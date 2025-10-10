62d3848f00765b46e9e88ebaa2a5e28b1f878be6
guido <70071968+punkedwerewolf@users.noreply.github.com>
2025-10-09 23:31:48 -0700

    d

---
+++ public/scripts/link.js
@@ -0,0 +1,7 @@
+ $(function(){
+      $("#left").load("/left.html");
+      $("#nav").load("/left.html");
+      $("#right").load("/right.html");
+      $("#footer").load("/footer.html");
+      $("#tos").load("/tos.html");
+    });
